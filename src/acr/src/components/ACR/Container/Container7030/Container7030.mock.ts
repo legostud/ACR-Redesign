445dc19a28d0d450ebff5d3194784be44a8d2c0f
Andrew Hoang <andrew.hoang@velir.com>
2024-05-08 14:16:27 -0700

    ACRAR-122 - Build Container 70-30

---
+++ src/acr/src/components/ACR/Container/Container7030/Container7030.mock.ts
@@ -0,0 +1,16 @@
+import { Container7030Props } from './Container7030.props';
+
+export const defaultMockData: Container7030Props = {
+  params: {
+    DynamicPlaceholderId: '1',
+  },
+  rendering: {
+    uid: '{00000000-0000-0000-0000-000000000000}',
+    componentName: 'Container7030',
+    dataSource: '/',
+    placeholders: {
+      'acr-container-70-left-1': [],
+      'acr-container-30-right-1': [],
+    },
+  },
+};
