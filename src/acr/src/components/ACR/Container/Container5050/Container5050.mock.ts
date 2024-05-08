f6116277a752e21ff9b9760b416580952687f69b
Andrew Hoang <andrew.hoang@velir.com>
2024-05-08 14:17:03 -0700

    ACRAR-121 - Build Container 50-50

---
+++ src/acr/src/components/ACR/Container/Container5050/Container5050.mock.ts
@@ -0,0 +1,16 @@
+import { Container5050Props } from './Container5050.props';
+
+export const defaultMockData: Container5050Props = {
+  params: {
+    DynamicPlaceholderId: '1',
+  },
+  rendering: {
+    uid: '{00000000-0000-0000-0000-000000000000}',
+    componentName: 'Container5050',
+    dataSource: '/',
+    placeholders: {
+      'acr-container-50-left-1': [],
+      'acr-container-50-right-1': [],
+    },
+  },
+};
---
+++ src/acr/src/components/ACR/Container/Container5050/Container5050.tsx
@@ -0,0 +1,69 @@
+import { Container, Grid, Section } from '@radix-ui/themes';
+import { useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
+
+import { Container5050Props } from 'components/ACR/Container/Container5050/Container5050.props';
+import {
+  getContainerPlaceholderProps,
+  isContainerPlaceholderEmpty,
+} from 'components/ACR/Container/Container.util';
+import PlaceholderBase, { PlaceholderBaseProps } from 'components/ACR/Placeholder/PlaceholderBase';
+import { twMerge } from 'tailwind-merge';
+
+const Container5050 = (props: Container5050Props): JSX.Element | null => {
+  const { rendering, componentFactory, left, right, testId } = props;
+
+  const { sitecoreContext } = useSitecoreContext();
+
+  const isPageEditing = sitecoreContext?.pageEditing ?? false;
+
+  const leftPlaceholders = getContainerPlaceholderProps('acr-container-50-left', props.params);
+  const rightPlaceholders = getContainerPlaceholderProps('acr-container-50-right', props.params);
+
+  const isEmptyPlaceholder =
+    isContainerPlaceholderEmpty(rendering, leftPlaceholders, left) &&
+    isContainerPlaceholderEmpty(rendering, rightPlaceholders, right);
+
+  if (isEmptyPlaceholder && !isPageEditing && !testId) {
+    return null;
+  }
+
+  const placeholderLeftProps: PlaceholderBaseProps = {
+    placeholder: {
+      name: leftPlaceholders.dynamicKey,
+      rendering,
+      componentFactory,
+    },
+    children: left,
+  };
+
+  const placeholderRightProps: PlaceholderBaseProps = {
+    placeholder: {
+      name: rightPlaceholders.dynamicKey,
+      rendering,
+      componentFactory,
+    },
+    children: right,
+  };
+
+  return (
+    <Section
+      pb="0"
+      pt="0"
+      className={twMerge('mt-[72px] first-of-type:mt-0', props.params.styles)}
+      data-testid={testId}
+    >
+      <Container px="6">
+        <Grid className="md:grid-cols-12" gap="6">
+          <Container className="md:col-start-1 md:col-end-7">
+            <PlaceholderBase {...placeholderLeftProps} />
+          </Container>
+          <Container className="md:col-start-7 md:col-end-13">
+            <PlaceholderBase {...placeholderRightProps} />
+          </Container>
+        </Grid>
+      </Container>
+    </Section>
+  );
+};
+
+export default Container5050;
