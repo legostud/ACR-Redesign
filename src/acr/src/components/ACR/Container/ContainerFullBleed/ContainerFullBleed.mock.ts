5eb07e1cfe508fcbc50eb6585d781276405133ab
Andrew Hoang <andrew.hoang@velir.com>
2024-05-13 09:48:35 -0700

    Update Global story styles

--- src/acr/src/components/ACR/Container/ContainerFullBleed/ContainerFullBleed.mock.ts
+++ src/acr/src/components/ACR/Container/ContainerFullBleed/ContainerFullBleed.mock.ts
@@ -4,6 +4,20 @@ import { ContainerFullBleedProps } from './ContainerFullBleed.props';
 export const defaultMockData: ContainerFullBleedProps = {
   params: {
     excludeTopMargin: '0',
+  },
+  rendering: {
+    uid: '{00000000-0000-0000-0000-000000000000}',
+    componentName: 'ContainerFullBleed',
+    dataSource: '/',
+    placeholders: {
+      'acr-container-fullbleed': [],
+    },
+  },
+};
+
+export const withBackgroundMockData: ContainerFullBleedProps = {
+  params: {
+    excludeTopMargin: '0',
     theme: Theme.LIGHT_INDIGO,
   },
   rendering: {
