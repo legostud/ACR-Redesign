66820f0d1c099187034d032a46e36cea8e108ae4
Andrew Hoang <andrew.hoang@velir.com>
2024-05-16 10:13:15 -0700

    ACRAR-356 - Style Image Animations

--- src/acr/src/components/ACR/Image/ImageBase.tsx
+++ src/acr/src/components/ACR/Image/ImageBase.tsx
@@ -1,14 +1,19 @@
+import { useContext, useState } from 'react';
 import { NextImage, ImageField } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ImageProps } from 'next/image';
-import { useContext } from 'react';
+
 import { ImageOptimizationContext } from 'src/context/ImageOptimization.context';
+
+import cn from 'classnames';
+import { useInView } from 'react-intersection-observer';
 
 type ImageBaseProps = Partial<ImageProps> & {
   image?: ImageField | null;
+  animate?: boolean;
 };
 
 const ImageBase = (props: ImageBaseProps) => {
-  const { image, className, ...rest } = props;
+  const { image, animate = true, className, ...rest } = props;
 
   const src = image?.value?.src;
 
@@ -18,12 +23,31 @@ const ImageBase = (props: ImageBaseProps) => {
 
   const { unoptimized } = useContext(ImageOptimizationContext);
 
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  const { ref, inView } = useInView({ triggerOnce: true });
+
   // SVGs by default should not be optimized
   // https://nextjs.org/docs/pages/api-reference/components/image#dangerouslyallowsvg
   const isSvg = src?.includes('.svg');
 
   return (
-    <NextImage field={image} className={className} unoptimized={unoptimized || isSvg} {...rest} />
+    <div ref={ref} className={cn('relative block max-w-max overflow-hidden', className)}>
+      <NextImage
+        field={image}
+        unoptimized={unoptimized || isSvg}
+        onLoad={() => setIsLoaded(true)}
+        {...rest}
+      />
+      {animate && (
+        <div
+          className={cn(
+            'absolute bottom-0 left-0 right-0 top-0 block origin-[50%_0] bg-green-100 transition-transform duration-1000',
+            { 'scale-y-0': isLoaded && inView }
+          )}
+        />
+      )}
+    </div>
   );
 };
 
