c9925201debb102ddb2f6436d0b87834b99d3adf
Minyie Diaz <minyie.diaz@velir.com>
2024-05-20 15:18:10 -0400

    ACRAR-106 : BE First Pass

--- src/acr/src/components/ACR/GlobalFooter/GlobalFooter.props.tsx
+++ src/acr/src/components/ACR/GlobalFooter/GlobalFooter.props.tsx
@@ -5,31 +5,26 @@ import { TestProps } from 'src/types/Test.props';
 /**
  * Model used for Sitecore Component integration
  */
-export type GlobalFooterProps = ComponentProps & TestProps &
-  GlobalFooterParams &
-  GlobalFooterFields &
-  GlobalFooterData;
-
-// Component Rendering Parameter fields
-// TODO_SCAFFOLD_BE: Populate if needed, remove if not
-export type GlobalFooterParams = {
-  params: {
-    mock_param?: string;
-  };
-};
-
-// Non-component data source fields
-// TODO_SCAFFOLD_BE: Populate if needed, remove if not
-export type GlobalFooterData = {
-  externalFields?: {
-    mock_external_data: Field<string>;
-  };
-};
+export type GlobalFooterProps = ComponentProps & TestProps & GlobalFooterFields;
 
 export type GlobalFooterFields = {
   fields: {
-    heading: Field<string>; // Sitecore editable text field
-    image?: ImageField; // Sitecore editable image field
-    link?: LinkField; // Sitecore editable link field
+    footerLogo: ImageField; // Sitecore editable text field
+    footerNewsletterText: Field<string>; // Sitecore editable text field
+    footerNewsletterLink: LinkField; // Sitecore editable text field
+    copyrightStatement: Field<string>; // Sitecore editable text field
+    directoryTitle: Field<string>; // Sitecore editable text field
+    directoryDescription: Field<string>; // Sitecore editable text field
+    directoryLink: LinkField; // Sitecore editable text field
+    engageTitle: Field<string>; // Sitecore editable text field
+    engageDescription: Field<string>; // Sitecore editable text field
+    engageLink: LinkField; // Sitecore editable text field
+    phoneNumber: Field<string>; // Sitecore editable text field
+    locationName: Field<string>; // Sitecore editable image field
+    address1: Field<string>;
+    address2: Field<string>;
+    city: Field<string>;
+    state: Field<string>;
+    zipcode: Field<string>;
   };
 };
