650c34eaa2d2f767412aed8815f4a6dcffe427c9
Minyie Diaz <minyie.diaz@velir.com>
2024-05-21 16:28:03 -0400

    ACRAR-269 : First BE Pass

--- src/acr/src/components/ACR/FacilitySearch/FacilitySearch.util.ts
+++ src/acr/src/components/ACR/FacilitySearch/FacilitySearch.util.ts
@@ -1,16 +1,56 @@
-import { ComponentRendering, LayoutServiceData } from '@sitecore-jss/sitecore-jss-react';
-import { FacilitySearchData } from 'components/ACR/FacilitySearch/FacilitySearch.props';
+import { ComponentRendering } from '@sitecore-jss/sitecore-jss-react';
+import {
+  FacilityEnumeration,
+  FacilitySearchData,
+  FacilitySearchDataModel,
+  FacilitySearchStateEnumGraphQL,
+} from 'components/ACR/FacilitySearch/FacilitySearch.props';
+import { ACREnumeration } from 'src/types/enum.props';
+import { GqlResult, getACRGraphQlClient } from 'src/utils/acrGraphQlClient';
+import { getStateCountryQuery } from './FacilitySearch.string';
 
+export type StateRequest = {
+  language?: string;
+  isPageEditing: boolean;
+};
 export const getStaticPropsForFacilitySearch = async (
   rendering: ComponentRendering,
-  layoutData: LayoutServiceData
+  request: StateRequest
 ): Promise<FacilitySearchData> => {
-  console.log(rendering);
-  console.log(layoutData);
+  const data = rendering as FacilitySearchDataModel;
+  const transformToFacilityEnumeration = (
+    item: ACREnumeration | FacilitySearchStateEnumGraphQL
+  ) => {
+    const model: FacilityEnumeration = {
+      name: item.name,
+      value: '',
+    };
+    if ('fields' in item) {
+      const m = item as ACREnumeration;
+      model.value = m?.fields?.Value?.value ?? '';
+    } else {
+      const m = item as FacilitySearchStateEnumGraphQL;
+      model.value = m?.value?.value ?? '';
+    }
+    return model;
+  };
+  const stateQuery = getStateCountryQuery(data?.fields?.stateOptions?.id, request.language);
+  const result =
+    await getACRGraphQlClient().request<GqlResult<FacilitySearchStateEnumGraphQL[]>>(stateQuery);
+
+  const countryQuery = getStateCountryQuery(data?.fields?.countryOptions?.id, request.language);
+  const countryResult =
+    await getACRGraphQlClient().request<GqlResult<FacilitySearchStateEnumGraphQL[]>>(countryQuery);
 
   const model: FacilitySearchData = {
     externalFields: {
-      mock_external_data: { value: 'Hardcoded for scaffolding' },
+      searchOptions: data?.fields?.searchOptions?.map(transformToFacilityEnumeration) ?? [],
+      designationOptions:
+        data?.fields?.designationOptions?.map(transformToFacilityEnumeration) ?? [],
+      milesOptions: data?.fields?.milesOptions?.map(transformToFacilityEnumeration) ?? [],
+      modalityOptions: data?.fields?.modalityOptions?.map(transformToFacilityEnumeration) ?? [],
+      stateOptions: result?.search?.results?.map(transformToFacilityEnumeration) ?? [],
+      countryOptions: countryResult?.search?.results?.map(transformToFacilityEnumeration) ?? [],
     },
   };
 
