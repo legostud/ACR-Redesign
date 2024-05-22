e4800ca1d057cc34699d5118135ee334c68a403c
Minyie Diaz <minyie.diaz@velir.com>
2024-05-22 13:12:14 -0400

    ACRAR-271 : BE Complete

--- src/acr/src/components/ACR/LinkList/LinkList.props.tsx
+++ src/acr/src/components/ACR/LinkList/LinkList.props.tsx
@@ -1,4 +1,4 @@
-import { Field, LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
+import { LinkField } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
 import { TestProps } from 'src/types/Test.props';
 import { ButtonStyle } from 'src/enumerations/ButtonStyle.enum';
@@ -8,31 +8,22 @@ import { ButtonStyle } from 'src/enumerations/ButtonStyle.enum';
  */
 export type LinkListProps = ComponentProps &
   TestProps &
-  LinkListParams &
-  LinkListFields &
-  LinkListData & {
+  LinkListFields & {
     hasIcon?: boolean;
     style?: ButtonStyle;
   };
 
-// Component Rendering Parameter fields
-// TODO_SCAFFOLD_BE: Populate if needed, remove if not
-export type LinkListParams = {
-  params: {
-    mock_param?: string;
-  };
-};
-
-// Non-component data source fields
-// TODO_SCAFFOLD_BE: Populate if needed, remove if not
-export type LinkListData = {
-  externalFields?: {
-    mock_external_data: Field<string>;
-  };
-};
-
 export type LinkListFields = {
   fields: {
-    links: LinkField[]; // Array of Sitecore editable link field objects
+    link1: LinkField;
+    link2: LinkField;
+    link3: LinkField;
+    link4: LinkField;
+    link5: LinkField;
+    link6: LinkField;
+    link7: LinkField;
+    link8: LinkField;
+    link9: LinkField;
+    link10: LinkField;
   };
 };
