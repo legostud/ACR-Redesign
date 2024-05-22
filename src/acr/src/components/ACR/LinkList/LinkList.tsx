807fdeb4e09c505c1280dd2ba0ad10fa14111a81
Minyie Diaz <minyie.diaz@velir.com>
2024-05-22 13:12:14 -0400

    ACRAR-271 : BE Complete

--- src/acr/src/components/ACR/LinkList/LinkList.tsx
+++ src/acr/src/components/ACR/LinkList/LinkList.tsx
@@ -10,6 +10,7 @@ import { ThemeContext } from 'src/context/Theme.context';
 import LinkBase from 'components/ACR/Link/LinkBase';
 
 import cn from 'classnames';
+import { LinkField, useSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs';
 
 /**
  * ACRAR-271 - Link List
@@ -18,7 +19,41 @@ import cn from 'classnames';
  */
 const LinkList = (props: LinkListProps): JSX.Element => {
   const { hasIcon = true, style = ButtonStyle.LINK, fields, testId } = props;
-  const { links } = fields ?? {};
+  const { sitecoreContext } = useSitecoreContext();
+  const isPageEditing = sitecoreContext?.pageEditing ?? false;
+
+  let links: LinkField[] = [];
+  if (isPageEditing) {
+    links = [
+      fields.link1,
+      fields.link2,
+      fields.link3,
+      fields.link4,
+      fields.link5,
+      fields.link6,
+      fields.link7,
+      fields.link8,
+      fields.link9,
+      fields.link10,
+    ];
+  } else {
+    const addLinkIfNotEmpty = (l: LinkField) => {
+      if (l?.value?.href) {
+        links.push(l);
+      }
+    };
+
+    addLinkIfNotEmpty(fields.link1);
+    addLinkIfNotEmpty(fields.link2);
+    addLinkIfNotEmpty(fields.link3);
+    addLinkIfNotEmpty(fields.link4);
+    addLinkIfNotEmpty(fields.link5);
+    addLinkIfNotEmpty(fields.link6);
+    addLinkIfNotEmpty(fields.link7);
+    addLinkIfNotEmpty(fields.link8);
+    addLinkIfNotEmpty(fields.link9);
+    addLinkIfNotEmpty(fields.link10);
+  }
 
   // Theme sourced from ContainerFullBleed
   const { theme } = useContext(ThemeContext);
