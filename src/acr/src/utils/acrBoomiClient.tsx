16f2eac8a17881a64eb6feac38fa8a5444342efd
Minyie Diaz <minyie.diaz@velir.com>
2024-05-23 15:56:02 -0400

    ACRAR-323 : Updates to the client

--- src/acr/src/utils/acrBoomiClient.tsx
+++ src/acr/src/utils/acrBoomiClient.tsx
@@ -4,31 +4,30 @@ export const getBoomiBaseApi = (): string => {
    return process.env.SALESFORCE_BASE_API_URL ?? config.boomiBaseApi;
  };*/
 
-import { AccountRequest } from "src/types/boomi/requests/AccountRequest.props";
+import { AccountRequest } from 'src/types/boomi/requests/AccountRequest.props';
+import { ACRAccount } from 'src/types/boomi/results/ACRAccount.props';
 
 const account = {
-   Account: {
-        Email_Address: 'akhanal@acr.org',
-        Okta_ID: '',
-        SF_Account_ID: ''
-      }};    
+  Account: {
+    Email_Address: 'akhanal@acr.org',
+    Okta_ID: '',
+    SF_Account_ID: '',
+  },
+};
 
-const addPosts = async (request:AccountRequest) => {
-   await fetch('https://acrapi-uat.acr.org:8443/ws/rest/getcustomer/v3/account/', {
-   method: 'POST',
-   body: JSON.stringify(account),
-   headers: { 
+export const getAccount = async (request: AccountRequest): Promise<ACRAccount> => {
+  const oktaAccessKey = process.env.OKTA_ACCESS_TOKEN ?? '';
+
+  const r = await fetch('https://acrapi-uat.acr.org:8443/ws/rest/getcustomer/v3/account/', {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers: {
       'Content-Type': 'application/json',
-      'x-api-key': '4b8373dd-bc52-47d0-bcb7-567959c8976c'
-   },
-   })
-   .then((response) => response.json())
-   .then((data) => {
-      setPosts((posts) => [data, ...posts]);
-      setTitle('');
-      setBody('');
-   })
-   .catch((err) => {
-      console.log(err.message);
-   });
-   };
+      'x-api-key': oktaAccessKey,
+    },
+  });
+  if (!r.ok) {
+    throw new Error(`Error in request getAccount ${r.statusText}`);
+  }
+  return await r.json().then((data: ACRAccount) => data);
+};
