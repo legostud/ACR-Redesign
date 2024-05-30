b660943fee6f1bea4c489f5c129b22ca09dbf155
Edwin-Naranjo <edwin.naranjo@velir.com>
2024-05-30 16:01:45 -0400

    ACRAR-106: Fix newsletter sign up styles

--- src/acr/src/components/ACR/GlobalFooter/GlobalFooter.tsx
+++ src/acr/src/components/ACR/GlobalFooter/GlobalFooter.tsx
@@ -1,8 +1,7 @@
-import { GlobalFooterProps, SocialLinks } from 'components/ACR/GlobalFooter/GlobalFooter.props';
+import { GlobalFooterProps, SocialLink } from 'components/ACR/GlobalFooter/GlobalFooter.props';
 import { PlaceholderBaseProps } from '../Placeholder/PlaceholderBase';
 import { Placeholder, NextImage } from '@sitecore-jss/sitecore-jss-nextjs';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
-import { Theme } from '@radix-ui/themes';
 import { Link } from '@sitecore-jss/sitecore-jss-react';
 
 import { Flex, Separator } from '@radix-ui/themes';
@@ -44,50 +43,46 @@ const GlobalFooter = (props: GlobalFooterProps): JSX.Element => {
 
   const { footerNewsletterText, footerNewsletterLink, footerLogo, socialLinks } = fields;
 
-  console.log("socialLinks: ", socialLinks);
-
   return (
-    <Theme data-theme="indigo" accentColor='green'>
-      <div data-ref="global-footer bg-t-background text-t-body" data-testid={testId}>
-        {footerNewsletterLink && footerNewsletterText && (
-          <div data-ref="global-footer__newsletter-sign-up indigo" data-testid={testId}>
-            <Flex justify="between" className='py-8'>
-              <Text tag="h4" className="heading-d green" field={footerNewsletterText} />
-              <LinkBase link={footerNewsletterLink} />
-            </Flex>
-            <Separator my="3" size="4" color='green' />
-          </div>
-        )}
-        <Flex>
+    <div data-ref="global-footer" data-testid={testId}>
+      {footerNewsletterLink && footerNewsletterText && (
+        <div data-ref="global-footer__newsletter-sign-up">
+          <Flex justify="between" className='py-8'>
+            <Text tag="h4" className="heading-d text-green-100" field={footerNewsletterText} />
+            <LinkBase link={footerNewsletterLink} styleClasses="bg-green-100 text-black" />
+          </Flex>
+          <Separator my="3" size="4" className='bg-green-100' />
+        </div>
+      )}
+      <Flex>
+        <div>
           <div>
-            <div>
-              <Link field={{ href: "/" }} aria-label='navigate to root site'>
-                <NextImage field={footerLogo} height="50" width="196" alt="american college of radiology logo" />
-              </Link>
-            </div>
-            <Flex gap="4" pt="5">
-              {socialLinks.map((socialLink: SocialLinks) => <FooterSocialIcon {...socialLink} iconClassName="h-6 w-6" />)}
-            </Flex>
+            <Link field={{ href: "/" }} aria-label='navigate to root site'>
+              <NextImage field={footerLogo} height="50" width="196" alt="american college of radiology logo" />
+            </Link>
           </div>
-          <Placeholder
-            name={footerLink1Placeholder.placeholder.name}
-            rendering={footerLink1Placeholder.placeholder.rendering}
-          />
-          <Placeholder
-            name={footerLink2Placeholder.placeholder.name}
-            rendering={footerLink1Placeholder.placeholder.rendering}
-          />
-        </Flex>
+          <Flex gap="4" pt="5">
+            {socialLinks.map((socialLink: SocialLink) => <FooterSocialIcon {...socialLink} iconClassName="h-6 w-6" />)}
+          </Flex>
+        </div>
         <Placeholder
-          name={footerLink3Placeholder.placeholder.name}
+          name={footerLink1Placeholder.placeholder.name}
           rendering={footerLink1Placeholder.placeholder.rendering}
         />
         <Placeholder
-          name={footerLink4Placeholder.placeholder.name}
-          rendering={footerLink4Placeholder.placeholder.rendering}
+          name={footerLink2Placeholder.placeholder.name}
+          rendering={footerLink1Placeholder.placeholder.rendering}
         />
-      </div>
-    </Theme>
+      </Flex>
+      <Placeholder
+        name={footerLink3Placeholder.placeholder.name}
+        rendering={footerLink1Placeholder.placeholder.rendering}
+      />
+      <Placeholder
+        name={footerLink4Placeholder.placeholder.name}
+        rendering={footerLink4Placeholder.placeholder.rendering}
+      />
+    </div>
   );
 };
 
