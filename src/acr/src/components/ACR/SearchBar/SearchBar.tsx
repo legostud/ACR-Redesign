c56365a6ab7b7acee492727e09852dc289b57dd7
Minyie Diaz <minyie.diaz@velir.com>
2024-05-30 17:27:26 -0400

    ACRAR-283 : Fix lint issues

--- src/acr/src/components/ACR/SearchBar/SearchBar.tsx
+++ src/acr/src/components/ACR/SearchBar/SearchBar.tsx
@@ -17,7 +17,7 @@ const SearchBar = (props: SearchBarProps): JSX.Element => {
 
   return (
     <div
-      className="mx-auto w-full max-w-[1268px] rounded-lg p-[30px] sm:px-[65px] sm:py-[41px] bg-t-background text-t-body"
+      className="mx-auto w-full max-w-[1268px] rounded-lg bg-t-background p-[30px] text-t-body sm:px-[65px] sm:py-[41px]"
       data-ref="searchbox"
       data-testid={testId}
     >
