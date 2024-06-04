542664b164f18136593709d3eadd1b22ea500801
Minyie Diaz <minyie.diaz@velir.com>
2024-06-04 12:59:16 -0400

    ACRAR-130 : Fix mock file

--- src/acr/src/components/ACR/Video/Video.mock.ts
+++ src/acr/src/components/ACR/Video/Video.mock.ts
@@ -8,6 +8,37 @@ export const defaultMockData: VideoProps = {
     dataSource: '/',
   },
   fields: {
-    heading: { value: 'MOCK_DATA_HEADING' },
+    coverImage: {
+      value: {
+        src: '/-/media/Feature/JSS-Experience-Accelerator/Basic-Site/banner-image.jpg?h=2001&iar=0&w=3000',
+        alt: '',
+        width: '3000',
+        height: '2001',
+      },
+    },
+    video: {
+      value: {
+        href: 'https://youtu.be/Z38JJpQ5mVE?si=hXWS_GMrtR_puriI',
+        text: 'Funny Cat',
+        linktype: 'external',
+        url: 'https://youtu.be/Z38JJpQ5mVE?si=hXWS_GMrtR_puriI',
+        anchor: '',
+        target: '',
+      },
+    },
+    caption: { value: 'Funny cat videos' },
+    transcript: {
+      value: {
+        src: '/-/media/Feature/ACR/Multimedia/image-thumbnail.png',
+        name: 'image-thumbnail',
+        displayName: 'image-thumbnail',
+        title: '',
+        keywords: '',
+        description: '',
+        extension: 'png',
+        mimeType: 'image/png',
+        size: '1356218',
+      },
+    },
   },
 };
