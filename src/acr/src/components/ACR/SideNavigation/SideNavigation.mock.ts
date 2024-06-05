c521fe4b0b75a2a2f06c2a2f4904c64a0d12899c
Minyie Diaz <minyie.diaz@velir.com>
2024-06-05 16:01:23 -0400

    ACRAR-341 : BE complete

--- src/acr/src/components/ACR/SideNavigation/SideNavigation.mock.ts
+++ src/acr/src/components/ACR/SideNavigation/SideNavigation.mock.ts
@@ -8,6 +8,44 @@ export const defaultMockData: SideNavigationProps = {
     dataSource: '/',
   },
   fields: {
-    heading: { value: 'MOCK_DATA_HEADING' },
+    title: { value: 'MOCK_DATA_HEADING' },
+    links: [
+      {
+        id: 'bb040074-e0da-41b3-8ec6-8fa69df79368',
+        url: '/SideNav/Data/SideNaveTest/Sub-Nav-1?sc_site=ACR',
+        name: 'Sub Nav 1',
+        displayName: 'Sub Nav 1',
+        fields: {
+          link: {
+            value: {
+              href: 'http://#',
+              text: 'Subnav 1',
+              linktype: 'external',
+              url: 'http://#',
+              anchor: '',
+              target: '',
+            },
+          },
+        },
+      },
+      {
+        id: '7a3bf269-5be2-4c93-a697-b81e70969468',
+        url: '/SideNav/Data/SideNaveTest/Sub-Nav-2?sc_site=ACR',
+        name: 'Sub Nav 2',
+        displayName: 'Sub Nav 2',
+        fields: {
+          link: {
+            value: {
+              href: 'http://#',
+              text: 'Subnav 2',
+              linktype: 'external',
+              url: 'http://#',
+              anchor: '',
+              target: '',
+            },
+          },
+        },
+      },
+    ],
   },
 };
--- src/acr/src/components/ACR/SideNavigation/SideNavigation.tsx
+++ src/acr/src/components/ACR/SideNavigation/SideNavigation.tsx
@@ -1,34 +1,31 @@
-import { GetStaticComponentProps } from '@sitecore-jss/sitecore-jss-nextjs';
 import { Text } from '@sitecore-jss/sitecore-jss-react';
 
-import { SideNavigationProps } from 'components/ACR/SideNavigation/SideNavigation.props';
-
-import { getStaticPropsForSideNavigation } from 'components/ACR/SideNavigation/SideNavigation.util';
+import {
+  SideNavigationLink,
+  SideNavigationProps,
+} from 'components/ACR/SideNavigation/SideNavigation.props';
+import LinkBase from '../Link/LinkBase';
 
 const SideNavigation = (props: SideNavigationProps): JSX.Element => {
   const { fields, testId } = props;
 
-  const { heading } = fields ?? {};
+  const { title, links } = fields ?? {};
 
   return (
     <div data-ref="side-navigation" data-testid={testId}>
-      <Text tag="h2" field={heading} />
       <p>The SideNavigation Component</p>
+      <Text tag="h2" field={title} />
+      {links && links?.length && (
+        <ul>
+          {links?.map((link: SideNavigationLink, index: number) => (
+            <li key={index}>
+              <LinkBase link={link?.fields?.link} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-};
-
-/**
- * "Data" developer method
- * TODO_SCAFFOLD_BE: If "getStaticProps" was deleted remove "useComponentProps". They work together.
- * TODO_SCAFFOLD_BE: Populate if needed, remove if not
- * Will be called during SSG.  Do NOT return null.
- * @param {ComponentRendering} _rendering
- * @param {LayoutServiceData} _layoutData
- * @param {GetStaticPropsContext} _context
- */
-export const getStaticProps: GetStaticComponentProps = async (_rendering, _layoutData) => {
-  return getStaticPropsForSideNavigation(_rendering, _layoutData);
 };
 
 export default SideNavigation;
