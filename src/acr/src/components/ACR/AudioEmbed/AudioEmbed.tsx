d4625a7bf0467b6399d89d49e921f73807b1ffa9
Minyie Diaz <minyie.diaz@velir.com>
2024-06-10 15:20:59 -0400

    ACRAR-281 : First FE Pass

--- src/acr/src/components/ACR/AudioEmbed/AudioEmbed.tsx
+++ src/acr/src/components/ACR/AudioEmbed/AudioEmbed.tsx
@@ -1,16 +1,20 @@
-import { Text } from '@sitecore-jss/sitecore-jss-react';
-
 import { AudioEmbedProps } from 'components/ACR/AudioEmbed/AudioEmbed.props';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import config from 'temp/config';
 
 const AudioEmbed = (props: AudioEmbedProps): JSX.Element => {
   const { fields, testId } = props;
-
+  const router = useRouter();
+  const publicUrl = config.publicUrl;
+  const currentPageUrl = `${publicUrl}${router.asPath}`;
+  const encodedUrl = encodeURI(currentPageUrl);
   const { playerId } = fields ?? {};
+  const url = `https://trinitymedia.ai/player/trinity/${playerId?.value}/?pageURL=${encodedUrl}`;
 
   return (
     <div data-ref="audio-embed" data-testid={testId}>
-      <Text tag="h2" field={playerId} />
-      <p>The AudioEmbed Component</p>
+      <Script src={url}></Script>
     </div>
   );
 };
